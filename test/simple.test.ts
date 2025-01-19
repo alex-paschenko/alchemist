358ed370b2d2989dd8e441c3c5e8790c42059f9d
Alexander Paschenko <alex.paschenko.first@gmail.com>
2025-01-19 07:32:24 +0200

    Fix jest coverage

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -17,4 +17,26 @@ describe('Simple merging', () => {
     expect(c.a).toBe("a");
     expect(c.b).toBe("b");
   });
+
+  it('should merge three classes', () => {
+    class A {
+      a = "a";
+    }
+
+    class B {
+      b = "b";
+    }
+
+    class C {
+      c = "c";
+    }
+
+    const D = alchemize(A, B, C);
+
+    const d = new D();
+
+    expect(d.a).toBe("a");
+    expect(d.b).toBe("b");
+    expect(d.c).toBe("c");
+  });
 });
