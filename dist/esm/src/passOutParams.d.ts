fc37b0792ec04750a5c5b7735a46a186656a4482
Alexander Paschenko <alex.paschenko.first@gmail.com>
2025-01-29 09:10:33 +0200

    Fix package integrity; add recipes

--- dist/esm/src/passOutParams.d.ts
+++
@@ -1,4 +0,0 @@
-import { PassOutParamRule } from './interfaces';
-declare function passOutSlices<T>(inputArray: T[], numOfClasses: number, passOutParamRules?: PassOutParamRule[]): T[][];
-declare function validatePassOutParamRules(passOutParamRules: PassOutParamRule[]): void;
-export { passOutSlices, validatePassOutParamRules };
